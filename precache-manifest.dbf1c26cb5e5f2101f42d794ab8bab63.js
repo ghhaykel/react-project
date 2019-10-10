740973bdd002e673a8b6e06578ce0f9505db6135
haykel <ghhaykel@gmail.com>
2019-10-10 06:36:05 +0200

    Updates

---
+++ precache-manifest.dbf1c26cb5e5f2101f42d794ab8bab63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "849e2764c791f6b152cad45c6c7157b5",
+    "url": "/react-project/index.html"
+  },
+  {
+    "revision": "7c9b7f11814f0f4b5ecf",
+    "url": "/react-project/static/css/main.edc6521c.chunk.css"
+  },
+  {
+    "revision": "c46f981124c7e2512171",
+    "url": "/react-project/static/js/2.90073e0d.chunk.js"
+  },
+  {
+    "revision": "7c9b7f11814f0f4b5ecf",
+    "url": "/react-project/static/js/main.317c8e24.chunk.js"
+  },
+  {
+    "revision": "ca46e0f517b98ed92b12",
+    "url": "/react-project/static/js/runtime-main.cd2cb06d.js"
+  }
+]);
